868a19b877da9ed0c316f2893b05c927e268bd5a
Dillon Fancher <daffancher@gmail.com>
2014-05-04 23:47:26 -0600

    almost there

--- LSA.py
+++ LSA.py
@@ -42,46 +42,15 @@ def corpusGen(tweet_clean_text):
     U, S, Vt = np.linalg.svd(tf_idf_MATRIX)
     print(np.shape(U))
     
-    k = len(S)
-    
-    for p in range(0, 10):
-        trunc = k-p
-        S = np.delete(S, (trunc-1), axis = 0)
-   
-    I = np.identity(len(S))
-    print(len(S))
-    
-    
-    Ulen = np.shape(U)[1]
-    Utrunk = Ulen-len(S)
-    for i in range(0, Utrunk):
-        j = Ulen - i
-        U = np.delete(U, (int(j)-1) , axis = 1)
-    
-    Vtlen = np.shape(Vt)[1]
-    Vtrunk = Vtlen - len(S) 
-    for m in range(0, Vtrunk):
-        n = Vtlen - m
-        Vt = np.delete(Vt, (n-1), axis = 0)
-    
-    print(np.shape(U))
-    print(np.shape(Vt))
-    print(np.shape(I))
-    inter = np.dot(U, I)
-    A = np.dot(inter, Vt)
-    
-    error = tf_idf_MATRIX - A
-    print(np.shape(error))
-    norm = LA.norm(error, 2)
-    print(norm)
    
   
   
-    #np.savetxt("TFIDF-Matrix.csv", tf_idf_MATRIX, delimiter=",")
-    #np.savetxt("U.csv", U, delimiter=",")
-    #np.savetxt("S.csv", S, delimiter=",")
-    #np.savetxt("Vt.csv", Vt, delimiter=",")
+    np.savetxt("TFIDF-Matrix.csv", tf_idf_MATRIX, delimiter=",")
+    np.savetxt("U.csv", U, delimiter=",")
+    np.savetxt("S.csv", S, delimiter=",")
+    np.savetxt("Vt.csv", Vt, delimiter=",")
     
+    print('worked')
     
     return U, S, Vt 
     
--- analyzeSVD.py
+++ analyzeSVD.py
@@ -22,7 +22,7 @@ tfIDF = np.genfromtxt("TFIDF-MATRIX.csv", delimiter = ",")
 
 #Truncate the SVD system
 k = len(S)
-for p in range(0, 97):
+for p in range(0, 1):
     trunc = k-p
     S = np.delete(S, (trunc-1), axis = 0)
 
@@ -39,10 +39,38 @@ for m in range(0, Vtrunk):
     Vt = np.delete(Vt, (n-1), axis = 0)
 
 #Re-Construct A from the truncated SVD system
-inter = np.dot(U, np.diag(S))
+Sk = np.diag(S)
+inter = np.dot(U, Sk)
 A = np.dot(inter, Vt)
-error = tfIDF - A
-norm = LA.norm(error, 2)
+#error = tfIDF - A
 
-print(np.column(A,2))
+#Check the error of the newly truncated matrix 
+#agianst the original
+#norm = LA.norm(error, 2)
+#print(np.column(A,2))
+docRel = np.dot(A.T, A)
+np.savetxt("docRel2.csv", docRel, delimiter = ",")
+
+print(np.shape(Sk))
+print(np.shape(Vt[:,0]))
+
+doc1 = np.dot(Sk, Vt[:, 1])
+doc2 = np.dot(Sk, Vt[:, 2])
+nd1 = LA.norm(doc1, 2)
+nd2 = LA.norm(doc2, 2)
+
+f1 = LA.norm(A, 'fro')
+f2 = LA.norm(tfIDF, 'fro')
+#print(f1/f2)
+#print(LA.norm(tfIDF-A, 'fro'))
+sim = np.dot(doc1, doc2)/(nd1*nd2)
+print(sim)
+
+
+
+
+
+
+
+
    
